ae1da4191537ae3f41da973988b31d1a37d9eef6
hp <hp@LAPTOP-DR5SM0AT>
2021-05-15 21:36:11 +0530

    initial commit

---
+++ grocery-project/src/main/java/GroceryProjectConstants.java
@@ -0,0 +1,12 @@
+
+public class GroceryProjectConstants {
+
+	public static final double SOUP_COST = 0.65;
+	public static final double BREAD_COST = 0.80;
+	public static final double MILK_COST = 1.30;
+	public static final double APPLE_COST = 0.10;
+	public static final String SOUP_PRODUCT = "soup";
+	public static final String BREAD_PRODUCT = "bread";
+	public static final String MILK_PRODUCT = "milk";
+	public static final String APPLE_PRODUCT = "apple";
+}
